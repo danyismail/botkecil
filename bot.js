20c9d3c528ce31824513806ac530ddd7a82a3b32
danyismail <dany_arie@yahoo.com>
2020-03-20 04:45:24 +0700

    benerin port

--- bot.js
+++ bot.js
@@ -35,4 +35,4 @@ bot.start();
 
 app.get('/', (req, res) => res.send('Bot kecil aktif'))
 
-app.listen(process.env.PORT || 5000, () => console.log(`botkecil listening on port ${port}!`))
+app.listen(process.env.PORT || 5000, () => console.log(`botkecil listening on port ${process.env.PORT}!`))
