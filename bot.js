23846dab882e952b7b8baf87e62d4b37f745e218
danyismail <dany_arie@yahoo.com>
2020-03-23 19:13:00 +0700

    nambahin keep alive

--- bot.js
+++ bot.js
@@ -48,6 +48,32 @@ bot.on('/covid19', (msg) => msg.reply.text(info + '\n' + source));
 
 bot.start();
 
+var http = require('http'); //importing http
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'botkecil.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
 app.get('/', (request, response) => response.send('Bot kecil aktif'))
 
 app.listen(process.env.PORT || 5000, () => console.log(`botkecil listening on port ${process.env.PORT}!`))
