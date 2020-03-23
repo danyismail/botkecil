0396c753e30925d94ae0dd82afdc9086471b242d
danyismail <dany_arie@yahoo.com>
2020-03-23 18:56:35 +0700

    benerin format display data

--- bot.js
+++ bot.js
@@ -7,32 +7,47 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const siteUrl = process.env.SITE_URL;
-
-let result = ''
-let arrResult = []
-let foreignTitle = ''
-let localTitle = ''
-let foreignData = ''
-let localData = ''
+arrResult = []
+dataGLobal = []
+dataLokal = []
+valDataGLobal = []
+valDataLokal = []
+info = ''
 let source = `Sumber data berasal dari situs ${process.env.SITE_URL}`
+
 axios(siteUrl)
       .then(response => {
         const html = response.data;
         const $ = cheerio.load(html);
-        result = $('.fusion-text p').each(function(){
+
+        $('p').each(function(){
           arrResult.push($(this).text())
         })
-        foreignTitle =  arrResult[0].substring(0,23) + ' : ' + '\n'
-        foreignData =  arrResult[0].substring(24) + '\n'
-        localTitle =  arrResult[1].substring(0,10) + ' : ' + '\n'
-        localData =  arrResult[1].substring(10) + '\n'
+        dataGLobal = arrResult[5].split(' ')
+        dataLokal = arrResult[6].split(' ')
+        valDataGLobal.push(dataGLobal[4])
+        valDataGLobal.push(dataGLobal[7])
+        valDataGLobal.push(dataGLobal[9])
+        valDataLokal.push(dataLokal[2])
+        valDataLokal.push(dataLokal[4])
+        valDataLokal.push(dataLokal[6])
+        info = `
+        Global
+        Negara / Kawasan : ${valDataGLobal[0]}
+        Kasus Terkonfirmasi : ${valDataGLobal[1]}
+        Kematian : ${valDataGLobal[2]}
+
+Indonesia
+        Positif : ${valDataLokal[0]}
+        Sembuh : ${valDataLokal[1]}
+        Meninggal : ${valDataLokal[2]}
+        `
       })
       .catch(console.error);
-
-bot.on('/covid19', (msg) => msg.reply.text(foreignTitle + foreignData + localTitle + localData + source));
+bot.on('/covid19', (msg) => msg.reply.text(info + '\n' + source));
 
 bot.start();
 
-app.get('/', (req, res) => res.send('Bot kecil aktif'))
+app.get('/', (request, response) => response.send('Bot kecil aktif'))
 
 app.listen(process.env.PORT || 5000, () => console.log(`botkecil listening on port ${process.env.PORT}!`))
